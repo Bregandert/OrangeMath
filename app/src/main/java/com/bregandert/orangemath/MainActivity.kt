c59a0480f1036e4dd07728df72bab11b31e5d4ae
Bregandert <bregandert84@gmail.com>
2024-05-20 19:40:49 +0300

    Создал фрагменты и навигацию между ними. DI не стал. Не вижу смысла в даггере для проекта

--- app/src/main/java/com/bregandert/orangemath/MainActivity.kt
+++ app/src/main/java/com/bregandert/orangemath/MainActivity.kt
@@ -3,6 +3,8 @@ package com.bregandert.orangemath
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -12,6 +14,7 @@ import com.bregandert.orangemath.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +24,12 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_settings
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
