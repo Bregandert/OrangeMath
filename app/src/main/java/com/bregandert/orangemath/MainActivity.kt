f213d58ded60f0226ed45ca99e69909ce93afc9b
Bregandert <bregandert84@gmail.com>
2024-06-01 16:29:13 +0300

    Ветка develop

--- app/src/main/java/com/bregandert/orangemath/MainActivity.kt
+++ app/src/main/java/com/bregandert/orangemath/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_settings
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
